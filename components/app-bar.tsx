7398662b5e2045c3a50bebbeb7be210d47e91436
kelani34 <kelanitaiwo34@gmail.com>
2023-11-03 23:55:17 +0100

    add: dock magnifier

--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -1,18 +1,57 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { scaleValue } from "@/utils/scale";
+import { useRef } from "react";
+
+const maxAdditionalSize = 5;
 export const AppBar = () => {
+  const dockRef = useRef<HTMLDivElement>(null);
+
+  const handleAppHover = (ev: React.MouseEvent<HTMLImageElement>) => {
+    if (!dockRef.current) return;
+
+    const mousePosition = ev.clientX;
+    const iconPositionLeft = ev.currentTarget.getBoundingClientRect().left;
+    const iconWidth = ev.currentTarget.getBoundingClientRect().width;
+
+    const cursorDistance = (mousePosition - iconPositionLeft) / iconWidth;
+    const offsetPixels = scaleValue(
+      cursorDistance,
+      [0, 1],
+      [maxAdditionalSize * -1, maxAdditionalSize]
+    );
+
+    dockRef.current.style.setProperty(
+      "--dock-offset-left",
+      `${offsetPixels * -1}px`
+    );
+
+    dockRef.current.style.setProperty(
+      "--dock-offset-right",
+      `${offsetPixels}px`
+    );
+  };
+
   return (
-    <div className=" absolute  bottom-4 left-1/2 transform -translate-x-1/2 p-2 bg-blend-multiply backdrop-blur-2xl rounded-[20px] bg-[rgba(245,245,245,0.15)] flex gap-2 items-center">
-      {[...Array(12).keys()].map((num) => (
-        <Image
-          key={num}
-          className="cursor-pointer"
-          src={`/images/app-bar/image-${num}.png`}
-          alt=""
-          height={62}
-          width={62}
-        />
-      ))}
+    <div
+      ref={dockRef}
+      className=" absolute  bottom-4 left-1/2 transform -translate-x-1/2 p-2 bg-blend-multiply backdrop-blur-2xl rounded-[20px] bg-[rgba(245,245,245,0.15)] "
+    >
+      <div className="dock-item flex gap-2 items-center">
+        {[...Array(12).keys()].map((num) => (
+          <div className="app" key={num} onMouseMove={handleAppHover}>
+            <Image
+              className="cursor-pointer "
+              src={`/images/app-bar/image-${num}.png`}
+              alt=""
+              height={62}
+              width={62}
+            />
+            <span className="tooltip text-xs">Arc Browser</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
