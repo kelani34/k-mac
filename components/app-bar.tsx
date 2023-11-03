9c9aa9c08cf431f519e60c77d5f2f65ecb2a92dd
kelani34 <kelanitaiwo34@gmail.com>
2023-11-04 00:24:12 +0100

    update: add dock data

--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { scaleValue } from "@/utils/scale";
 import { useRef } from "react";
+import { dockItems } from "@/data/dock-items";
 
 const maxAdditionalSize = 5;
 export const AppBar = () => {
@@ -36,19 +37,23 @@ export const AppBar = () => {
   return (
     <div
       ref={dockRef}
-      className=" absolute  bottom-4 left-1/2 transform -translate-x-1/2 p-2 bg-blend-multiply backdrop-blur-2xl rounded-[20px] bg-[rgba(245,245,245,0.15)] "
+      className=" absolute  bottom-4 left-1/2 transform -translate-x-1/2 p-2  bg-blend-multiply backdrop-blur-2xl rounded-[20px] bg-[rgba(245,245,245,0.15)] "
     >
       <div className="dock-item flex gap-2 items-center">
-        {[...Array(12).keys()].map((num) => (
-          <div className="app" key={num} onMouseMove={handleAppHover}>
+        {dockItems.map((dockItem) => (
+          <div
+            className="app box-border"
+            key={dockItem.id}
+            onMouseMove={handleAppHover}
+          >
             <Image
-              className="cursor-pointer "
-              src={`/images/app-bar/image-${num}.png`}
+              className="cursor-pointer"
+              src={dockItem.src}
               alt=""
               height={62}
               width={62}
             />
-            <span className="tooltip text-xs">Arc Browser</span>
+            <span className="tooltip text-xs">{dockItem.tooltip}</span>
           </div>
         ))}
       </div>
---
+++ data/dock-items.ts
@@ -0,0 +1,62 @@
+export const dockItems = [
+  {
+    id: 0,
+    src: "/images/app-bar/image-0.png",
+    tooltip: "Finder",
+  },
+  {
+    id: 1,
+    src: "/images/app-bar/image-1.png",
+    tooltip: "Chrome",
+  },
+  {
+    id: 2,
+    src: "/images/app-bar/image-2.png",
+    tooltip: "AppStore",
+  },
+  {
+    id: 3,
+    src: "/images/app-bar/image-3.png",
+    tooltip: "Messages",
+  },
+  {
+    id: 4,
+    src: "/images/app-bar/image-4.png",
+    tooltip: "Maps",
+  },
+  {
+    id: 5,
+    src: "/images/app-bar/image-5.png",
+    tooltip: "Contacts",
+  },
+  {
+    id: 6,
+    src: "/images/app-bar/image-6.png",
+    tooltip: "Mails",
+  },
+  {
+    id: 7,
+    src: "/images/app-bar/image-7.png",
+    tooltip: "Podcast",
+  },
+  {
+    id: 8,
+    src: "/images/app-bar/image-8.png",
+    tooltip: "Reminders",
+  },
+  {
+    id: 9,
+    src: "/images/app-bar/image-9.png",
+    tooltip: "Notes",
+  },
+  {
+    id: 10,
+    src: "/images/app-bar/image-10.png",
+    tooltip: "Settings",
+  },
+  {
+    id: 11,
+    src: "/images/app-bar/image-11.png",
+    tooltip: "Bin",
+  },
+];
