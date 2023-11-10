b00c43ea08637516709ae845e411cf92b872ae55
kelani34 <kelanitaiwo34@gmail.com>
2023-11-10 20:30:32 +0100

    update: image naming error

--- data/dock-items.ts
+++ data/dock-items.ts
@@ -1,62 +1,62 @@
 export const dockItems = [
   {
     id: 0,
-    src: "/images/app-bar/image-0.png",
+    src: "/images/app-bar/Image-0.png",
     tooltip: "Finder",
   },
   {
     id: 1,
-    src: "/images/app-bar/image-1.png",
+    src: "/images/app-bar/Image-1.png",
     tooltip: "Chrome",
   },
   {
     id: 2,
-    src: "/images/app-bar/image-2.png",
+    src: "/images/app-bar/Image-2.png",
     tooltip: "AppStore",
   },
   {
     id: 3,
-    src: "/images/app-bar/image-3.png",
+    src: "/images/app-bar/Image-3.png",
     tooltip: "Messages",
   },
   {
     id: 4,
-    src: "/images/app-bar/image-4.png",
+    src: "/images/app-bar/Image-4.png",
     tooltip: "Maps",
   },
   {
     id: 5,
-    src: "/images/app-bar/image-5.png",
+    src: "/images/app-bar/Image-5.png",
     tooltip: "Contacts",
   },
   {
     id: 6,
-    src: "/images/app-bar/image-6.png",
+    src: "/images/app-bar/Image-6.png",
     tooltip: "Mails",
   },
   {
     id: 7,
-    src: "/images/app-bar/image-7.png",
+    src: "/images/app-bar/Image-7.png",
     tooltip: "Podcast",
   },
   {
     id: 8,
-    src: "/images/app-bar/image-8.png",
+    src: "/images/app-bar/Image-8.png",
     tooltip: "Reminders",
   },
   {
     id: 9,
-    src: "/images/app-bar/image-9.png",
+    src: "/images/app-bar/Image-9.png",
     tooltip: "Notes",
   },
   {
     id: 10,
-    src: "/images/app-bar/image-10.png",
+    src: "/images/app-bar/Image-10.png",
     tooltip: "Settings",
   },
   {
     id: 11,
-    src: "/images/app-bar/image-11.png",
+    src: "/images/app-bar/Image-11.png",
     tooltip: "Bin",
   },
 ];
